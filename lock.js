276acfb2f640c13d673f34abbec8fac71bde7b98
VΛLUΛRD <150893297+tdwng@users.noreply.github.com>
2025-02-17 04:03:09 +0700

    Update lock.js

--- lock.js
+++ lock.js
@@ -4,7 +4,6 @@ const correctPassword = [10, 12, 10, 11, 11];
 function checkPassword() {
     const dials = document.querySelectorAll('.dial');
     const enteredPassword = Array.from(dials).map(dial => Number(dial.value));
-    
     const decodedPassword = [
         enteredPassword[0] + enteredPassword[1] + enteredPassword[2],
         enteredPassword[1] + enteredPassword[2] + enteredPassword[3],
@@ -12,19 +11,12 @@ function checkPassword() {
         enteredPassword[3] + enteredPassword[4] + enteredPassword[0],
         enteredPassword[4] + enteredPassword[0] + enteredPassword[1],
     ];
-    
     if (JSON.stringify(decodedPassword) === JSON.stringify(correctPassword)) {
-        const videoURL = "https://raw.githubusercontent.com/tdwng/bksec/main/jslock_chill/Lock_chill/sieucapdeptrai.mp4"; 
-        const videoContainer = document.getElementById("video-container");
-        videoContainer.innerHTML = `
-            <video controls autoplay width="640">
-                <source src="${videoURL}" type="video/mp4">
-                Trình duyệt bro lỏ quá nên không hiển thị dc flag
-            </video>
-        `;
-        const videoElement = videoContainer.querySelector("video");
-        videoElement.requestFullscreen?.();
+        const passwordString = enteredPassword.join('');
+        const hexPassword = new TextEncoder().encode(passwordString).reduce((prev, curr) => prev + curr.toString(16).padStart(2, '0'), '');
+        const base64Password = btoa(hexPassword);
+        alert("Congratulation! Your password is : BKSEC{" + base64Password + "}");
     } else {
-        alert("Password không chính xác. Vui lòng thử lại.");
+        alert('Wrong password! Let u cook!');
     }
 }
